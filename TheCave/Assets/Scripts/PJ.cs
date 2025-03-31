73b88d1e6ad0e4a67bc5fc5e4742214483eca197
Hernán Bravo <72671694+CBreaker96@users.noreply.github.com>
2025-03-31 16:18:37 -0300

    Pro Builder
    Se inició el nivel con ProBuilder y se refinaron los controles

--- TheCave/Assets/Scripts/PJ.cs
+++ TheCave/Assets/Scripts/PJ.cs
@@ -5,49 +5,83 @@ using UnityEngine;
 public class PJ : MonoBehaviour
 {
     [SerializeField]
-    float maximaVelocidad;
-
+    private Transform _camTransform;
     [SerializeField]
-    float fuerzaSalto;
-
+    float _movementSpeed;
     [SerializeField]
-    float modificadorVelocidad;
-
-    bool piso;
+    float _rotationSpeed;
+    [SerializeField]
+    float _jumpForce;
+    [SerializeField]
+    float _speedMultiplier;
+    [SerializeField]
+    bool _grounded;
 
     Rigidbody rb;
 
-
+    //Vector de dirección
+    private Vector3 _direction;
+    
+    //Vector indicador de posición de la cámara
+    private Vector3 _camRel;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        
     }
 
     void Update()
     {
-        if (piso) MoviendoPj(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-        if (Input.GetKeyDown(KeyCode.Space) && piso) Jump();
+        float _moveonX = (Input.GetAxisRaw("Horizontal")); 
+        float _moveonZ = (Input.GetAxisRaw("Vertical"));
+        _direction = new Vector3(_moveonX, 0 , _moveonZ);
+        if (Input.GetKeyDown(KeyCode.Space) && _grounded) Jump();
+        Ducking();
     }
 
-    void MoviendoPj(float horizontal, float vertical)
+    private void FixedUpdate()
     {
-        //Hacemos que el personaje pueda moverse, de manera horizontal como vertical
-        Vector3 direccion = new Vector3(horizontal, 0, vertical).normalized;
+        //Si el jugador se mueve, el modelo siempre apuntará en la dirección que se está moviendo.
+        if(_direction.sqrMagnitude != 0)
+        {
+            Movement(_direction);  
+        }  
+    }
 
-        rb.MovePosition(transform.position + direccion * maximaVelocidad * Time.deltaTime);
+    void Movement(Vector3 direction)
+    {
+        //Iguala los ejes del personaje a los de la cámara.
+        Vector3 fwd = _camTransform.forward;
+        Vector3 rht = _camTransform.right;
+        fwd.y = 0;
+        rht.y = 0;
+        fwd = fwd.normalized;
+        rht = rht.normalized;
+        Vector3 fwdRel = _direction.z * fwd;
+        Vector3 rhtRel = _direction.x * rht;
+        _camRel = fwdRel + rhtRel;
+        //
 
-        //El personaje con esta condicion tiene la opcion de poder ir agachado en el juego como si estuviera en sigilo
+        rb.MovePosition(transform.position + _camRel.normalized * _movementSpeed * Time.fixedDeltaTime);
 
-        if (Input.GetKeyDown(KeyCode.LeftControl)) maximaVelocidad /= modificadorVelocidad;
-
-        else if (Input.GetKeyUp(KeyCode.LeftControl)) maximaVelocidad *= modificadorVelocidad;
+        //Establece la rotación del personaje siempre hacia donde se esté moviendo, pero relativo a la cámara.
+        Quaternion rotateTo = Quaternion.LookRotation(_camRel, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotateTo, _rotationSpeed * Time.fixedDeltaTime);
     }
 
     void Jump()
     {
         //Realizamos que el personaje pueda saltar.
-        rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+    }
+
+    void Ducking()
+    {
+        //El personaje con esta condicion tiene la opcion de poder ir agachado en el juego como si estuviera en sigilo
+
+        if (Input.GetKeyDown(KeyCode.LeftControl)) _movementSpeed /= _speedMultiplier;
+
+        else if (Input.GetKeyUp(KeyCode.LeftControl)) _movementSpeed *= _speedMultiplier;
     }
 
     //Evitamos que el jugador siga saltando en el aire y no este volando.
@@ -55,14 +89,14 @@ public class PJ : MonoBehaviour
     {
         if (collision.gameObject.layer == 3)
         {
-            piso = true;
+            _grounded = true;
         }
     }
     private void OnCollisionExit(Collision collision)
     {
         if (collision.gameObject.layer == 3)
         {
-            piso = false;
+            _grounded = false;
         }
     }
 }
